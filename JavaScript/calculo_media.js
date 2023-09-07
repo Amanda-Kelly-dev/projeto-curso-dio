3a4250b52954bea07dbd64168003916734ed21dc
Amanda Kelly Ferreira <142455871+Amanda-Kelly-dev@users.noreply.github.com>
2023-09-06 22:23:26 -0300

    Create calculo_media.js
    Praticando calculo de media no Java Script

---
+++ JavaScript/calculo_media.js
@@ -0,0 +1,32 @@
+//Faça um algoritimo que dado as 3 notas tiradas por um aluno em um semestre
+//da faculdade, calcule e imprima a sua media e a sua classificação conforme tabela.
+
+// Media = (nota1 + nota2 + nota3) / 3
+
+//Classificacão
+//-media  menor que 5, reprovação
+//-media entre 5 e 7, recuperacao
+//- media acima de 7, passou de FileSystemWritableFileStream
+
+const nota1 = 5;
+const nota2 = 7;
+const nota3 = 9;
+const media = (nota1 + nota2 + nota3) / 3;
+const resultado = media
+
+console.log(media);
+
+if (media <= 5) {
+    console.log("Reprovado");
+} else if (media >= 5 && media <= 7) {
+    console.log("recuperação");
+} else {
+    console.log("Passou de Semestre")
+}
+
+const numero = 5;
+if (numero % 2 ===)) {
+    console.log('numero par');
+}else {
+    console.log('numero impar');
+}
